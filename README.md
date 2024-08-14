108d91ef52b8ac3df5e72bf2953bc1496059717e
nicknuzhny <120589745+nicknuzhny@users.noreply.github.com>
2024-08-13 22:07:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Locale Lending Platform
---
+++ src/pages/Borrow.js
@@ -0,0 +1,11 @@
+function Borrow() {
+  return (
+    <div className="borrow">
+      <h1>Borrow</h1>
+      <h2>Credit Lines</h2>
+      <p>You do not have any credit lines. To borrow funds from the pool, you needd a LocaleLending credit line.</p>
+    </div>
+  );
+}
+
+export default Borrow;
